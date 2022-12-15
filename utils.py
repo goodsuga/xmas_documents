2128a1895e540db965ee22f8618760251c78ab01
Daniil Raspopin <daniiltranslator@gmail.com>
2022-12-15 22:21:52 +0300

    Сделал склеивалку датасета

--- utils.py
+++ utils.py
@@ -1,21 +1,31 @@
 # Файл для утилит
 from pathlib import Path
-TEST_PATH = r"C:\Users\teberda\Documents\GitHub\xmas_documents\docs\0b4be82b86eff410d69d1d6b5553d220.docx"
 import tika
 tika.initVM()
 from tika import parser
 
 import flaml
 import pandas as pd
+from tqdm import tqdm
+import json
 
-def get_document_text(path: Path) -> str:
+def get_document_text(path: str) -> str:
     parsed = parser.from_file(path)
-    print(type(parsed['metadata']))
-    print(type(parsed['content']))
+    return parsed['content']
 
 
-def make_train_dataset(data_dir: Path) -> pd.DataFrame:
-    pass
+def make_train_dataset(data_dir: Path, class_file: Path) -> pd.DataFrame:
+    with open(class_file, "r", encoding='utf-8') as read_classes:
+        class_data = json.loads(read_classes.read())
+    
+    rows = [
+        {
+            "Класс документа": class_data[file.name],
+            "Текст документа": get_document_text(str(file))
+        }
+        for file in tqdm(map(Path, data_dir.glob("*")))
+    ]
+    return pd.DataFrame(rows)
 
-
-get_document_text(TEST_PATH)
+CLASS_PATH = r"C:\Users\teberda\Documents\GitHub\xmas_documents\classes.json"
+print(make_train_dataset(Path(r"C:\Users\teberda\Documents\GitHub\xmas_documents\docs"), CLASS_PATH))
