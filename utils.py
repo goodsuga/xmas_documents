9fc36d986adba9d9cbc858bacc6c4ffa6e7f6e98
Daniil Raspopin <daniiltranslator@gmail.com>
2022-12-15 22:29:13 +0300

    Добавил уборку мусорных символов

--- utils.py
+++ utils.py
@@ -3,11 +3,13 @@ from pathlib import Path
 import tika
 tika.initVM()
 from tika import parser
+import re
 
 import flaml
 import pandas as pd
 from tqdm import tqdm
 import json
+from typing import Iterable, List
 
 def get_document_text(path: str) -> str:
     parsed = parser.from_file(path)
@@ -27,5 +29,24 @@ def make_train_dataset(data_dir: Path, class_file: Path) -> pd.DataFrame:
     ]
     return pd.DataFrame(rows)
 
-CLASS_PATH = r"C:\Users\teberda\Documents\GitHub\xmas_documents\classes.json"
-print(make_train_dataset(Path(r"C:\Users\teberda\Documents\GitHub\xmas_documents\docs"), CLASS_PATH))
+#CLASS_PATH = r"C:\Users\teberda\Documents\GitHub\xmas_documents\classes.json"
+#ds = make_train_dataset(Path(r"C:\Users\teberda\Documents\GitHub\xmas_documents\docs"), CLASS_PATH)
+#ds.to_parquet("train.pqt")
+
+def clear_texts(texts: Iterable) -> 'List[str]':
+    """
+    Чистим текст от мусора в виде служебных символов
+    """
+    allowed_chars = " 0123456789абвгдеёжзийклмонпрстуфхцчшщъыьэюяabcdefghijklmnopqrstuvwxyz"
+    def preprocess(text):
+        clear = text.replace("\n", " ").lower()
+        clear = re.sub(" +", " ", clear)
+        clear = "".join([c for c in clear if c in allowed_chars])
+        return clear
+    
+    return list(map(preprocess, tqdm(texts)))
+
+
+#data = pd.read_parquet("train.pqt")
+#data.loc[:, "Текст документа"] = clear_texts(data['Текст документа'])
+#data.to_parquet("train_no_trash.pqt")
